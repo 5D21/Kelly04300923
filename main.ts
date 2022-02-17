793ccd7ba5ff2b876e776af2949a896872b54696
5D21 <98442529+5D21@users.noreply.github.com>
2022-02-17 12:03:28 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+input.onSound(DetectedSound.Loud, function () {
+    soundExpression.giggle.playUntilDone()
+})
+basic.forever(function () {
+	
+})
